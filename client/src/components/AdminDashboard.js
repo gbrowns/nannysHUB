6b97da68c04ad55d6023b56b9a6bbed41cb9d5d7
Godwin254 <godwinbee15@gmail.com>
2022-08-17 23:28:28 +0300

    add admin dashboard implementation

--- client/src/components/AdminDashboard.js
+++ client/src/components/AdminDashboard.js
@@ -1,8 +1,156 @@
-import React from 'react'
+import React from 'react';
+import { FaUsers } from 'react-icons/fa';
+import { TbChevronDown } from 'react-icons/tb';
+import { RiAdminFill } from 'react-icons/ri';
+import { MdChildFriendly, MdWork, MdAdminPanelSettings } from 'react-icons/md';
+
 
 function AdminDashboard() {
   return (
-    <div>AdminDashboard</div>
+    <div class="admin-dashboard">
+        <nav>
+            <div className="logo">
+                <img src={require('../images/logo.png')} alt="logo" width="150px" />
+            </div>
+
+            <div className="nav-links">
+                <div className="profile">
+                    <RiAdminFill className='admin-icon' />
+                    <span>Admin</span>
+                    <TbChevronDown className='icon'/>
+                </div> 
+            </div>
+
+        </nav>
+        <main>
+            <div class="title">
+                <MdAdminPanelSettings className='icon'/>
+                <h1>Admin Dashboard</h1>
+            </div>
+            <div class="cards">
+                <div class="card"> 
+                    <FaUsers className='icon'/>
+                    <h2>Total Requests</h2>
+                    <p>33</p>
+                    <span>Last updated today</span>
+                </div>
+                <div class="card">
+                    <MdWork className='icon' />
+                    <h2>Clients messages</h2>
+                    <p>33</p>
+                    <span>Last updated today</span>
+                </div>
+                <div class="card">
+                    <MdChildFriendly className='icon' />
+                    <h2>Available Nannies</h2>
+                    <p>33</p>
+                    <span>Last updated today</span>
+                </div>
+
+            </div>
+            <section>
+                <div className='left'>
+                    <h2>Registration Requests</h2>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Phone</th>
+                                <th>Email</th>
+                                <th>Location</th>
+                                <th>Contract</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>John Doe</td>
+                                <td>0712345678</td>
+                                <td>Johndoe@gmail.com</td>
+                                <td>Nairobi</td>
+                                <td>Full Time</td>
+                                <td>
+                                    <input type="button" value="View details" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>John Doe</td>
+                                <td>0712345678</td>
+                                <td>Johndoe@gmail.com</td>
+                                <td>Nairobi</td>
+                                <td>Full Time</td>
+                                <td>
+                                    <input type="button" value="View details" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>John Doe</td>
+                                <td>0712345678</td>
+                                <td>Johndoe@gmail.com</td>
+                                <td>Nairobi</td>
+                                <td>Full Time</td>
+                                <td>
+                                    <input type="button" value="View details" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>John Doe</td>
+                                <td>0712345678</td>
+                                <td>Johndoe@gmail.com</td>
+                                <td>Nairobi</td>
+                                <td>Full Time</td>
+                                <td>
+                                    <input type="button" value="View details" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>John Doe</td>
+                                <td>0712345678</td>
+                                <td>Johndoe@gmail.com</td>
+                                <td>Nairobi</td>
+                                <td>Full Time</td>
+                                <td>
+                                    <input type="button" value="View details" />
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div className='right'>
+                    <h2>Clients List</h2>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Location</th>
+                                <th>Message</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>John Doe</td>
+                                <td>johndoe@gmail.com</td>
+                                <td>Lavington</td>
+                                <td>
+                                    <input type="button" value="Message" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>John Doe</td>
+                                <td>johndoe@gmail.com</td>
+                                <td>Syokimau</td>
+                                <td>
+                                    <input type="button" value="Message" />
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </section>
+        </main>
+
+    </div>
   )
 }
 
