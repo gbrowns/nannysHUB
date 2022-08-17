7c271834bfbfdd193be3bd5c98d93ed939b248c7
Godwin254 <godwinbee15@gmail.com>
2022-08-17 17:16:57 +0300

    modify code base

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './scss/App.scss';
 import Admin from './views/Admin';
+import Home from './views/Home';
+import AdminLogin from './components/AdminLogin';
+import AdminDashboard from './components/AdminDashboard';
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Admin />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path="admin" element={<Admin />}/>
+        <Route path="login" element={<AdminLogin />} />
+        <Route path="dashboard" element={<AdminDashboard />} />
+      </Routes>
     </div>
   );
 }
--- client/src/views/Admin.js
+++ client/src/views/Admin.js
@@ -1,10 +1,14 @@
-import React from 'react'
-import AdminLogin from '../components/AdminLogin'
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+import AdminDashboard from '../components/AdminDashboard';
+import AdminLogin from '../components/AdminLogin';
 
+//admin view
 function Admin() {
+
   return (
     <div className="admin-view">
-        <AdminLogin />
+      <h1>Hello Admin</h1>
     </div>
   )
 }
