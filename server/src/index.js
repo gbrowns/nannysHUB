0e6d9ce37b73555be9497a5cee40b0d580eddba2
Godwin254 <godwinbee15@gmail.com>
2022-08-24 23:10:22 +0300

    modify codebase

--- server/src/index.js
+++ server/src/index.js
@@ -4,9 +4,6 @@ const app = express();
 const PORT = 8000;
 const LOCALHOST = `http://localhost:${PORT}`;
 
-app.get("/", (req, res) => {
-    res.send("<h2>Nanny's Server Started</h2>");
-})
 
 app.listen(PORT, (req, res)=>{
     console.log(`Server is running on ${LOCALHOST}`);
