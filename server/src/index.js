8851e736c29ebaafc44978cda817cced8252647b
Godwin254 <godwinbee15@gmail.com>
2022-08-24 23:15:01 +0300

    initial cors

--- server/src/index.js
+++ server/src/index.js
@@ -4,6 +4,11 @@ const app = express();
 const PORT = 8000;
 const LOCALHOST = `http://localhost:${PORT}`;
 
+const cors = require('cors');
+
+//middleware
+app.use(cors());
+
 
 app.listen(PORT, (req, res)=>{
     console.log(`Server is running on ${LOCALHOST}`);
