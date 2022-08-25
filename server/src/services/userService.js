962e820426107435b1425882df8f6f084f462e4e
Godwin254 <godwinbee15@gmail.com>
2022-08-25 16:49:49 +0300

    add messages service

---
+++ server/src/services/userService.js
@@ -0,0 +1,59 @@
+const {v4: uuid} = require('uuid');
+//require model
+const Message = require("../database/Message");
+
+//handle messages
+const getAllMessages = () => {
+   try{
+    const allMessages = Message.getAllMessages();
+
+    return allMessages;
+
+   }catch(error){
+        throw error;
+   }
+}
+
+const getOneMessage = (messageId) => {
+    try{
+        const oneMessage = Message.getOneMessages(messageId);
+        return oneMessage;
+    }catch(error){
+        throw error;
+    }
+}
+
+const createNewMessage = (newMessage) => {
+
+    const messageToInsert = {
+        ...newMessage,
+        id: uuid(),
+        sentAt: new Date().toLocaleTimeString,
+        updatedAt: new Date().toLocaleTimeString
+    }
+
+    try{
+        const createdMessage = Message.createNewMessage(messageToInsert);
+        return createdMessage;
+    }catch(error){
+        throw error;
+    }
+}
+
+const updateOneMessage = (messageId, change) => {
+    try{
+        const updatedMessage = Message.updateOneMessage(messageId, change);
+        return updatedMessage;
+    }catch(error){
+        throw error;
+    }
+}
+
+const deleteOneMessage = (messageId) => {
+    try{
+        const messageDeleted = Message.deleteOneMessage(messageId);
+        return messageDeleted;
+    }catch(error){
+        throw error;
+    }
+}
