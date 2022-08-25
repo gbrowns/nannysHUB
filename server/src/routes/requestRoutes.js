c1e31a19d8ad640c4759f4c784911453ce5700b6
Godwin254 <godwinbee15@gmail.com>
2022-08-25 15:10:42 +0300

    modify requests router

---
+++ server/src/routes/requestRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+//require controllers
+const requestController = require("../controllers/requestController");
+
+//crud operations
+router.get("/", requestController.getAllRequests);
+
+router.get("/:requestId", requestController.getOneRequest);
+
+router.post("/", requestController.createNewNanny);
+
+router.put("/:requestId", requestController.updateOneRequest);
+
+router.delete("/:requestId", requestController.deleteOneRequest);
