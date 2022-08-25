cab46d02602e1c0c488136146b86383a3fe0c6e4
Godwin254 <godwinbee15@gmail.com>
2022-08-25 13:25:11 +0300

    add toggle to show and hide  navlinks

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,14 +2,23 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import {BiMenuAltRight} from 'react-icons/bi'
 import { GrFormClose } from 'react-icons/gr'
+import {useState} from 'react'
 
 function Navbar() {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const handleToggle = () => {
+      setIsOpen(!isOpen)
+      console.log("isOpen", isOpen)
+    }
+
+
   return (
     <div className="navbar">
         <div className="logo">
             <img src={require('../images/logo.png')} alt="logo" width="150px" />
         </div>
-        <div className="nav-links">
+        <div className="nav-links" style={{display: isOpen ? "none" : "flex"}}>
             <Link to="/">Home</Link>
             <Link to="/about-us">About us</Link>
             <Link to="/contact-us">Contact us</Link>
@@ -17,7 +26,7 @@ function Navbar() {
             <Link to="/apply-for-job">Apply for job</Link>
         </div>
         <div className="menu-controls">
-            <BiMenuAltRight className="menu-icon"/>
+            <BiMenuAltRight className="menu-icon" onClick={handleToggle}/>
             <GrFormClose className="close-icon"/>
         </div>
 
