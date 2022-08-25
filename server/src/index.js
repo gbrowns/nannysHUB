1905bf124f939d0b3fc01facc9bbad32e63abfeb
Godwin254 <godwinbee15@gmail.com>
2022-08-25 15:13:38 +0300

    require routes in the index file

--- server/src/index.js
+++ server/src/index.js
@@ -7,15 +7,17 @@ const LOCALHOST = `http://localhost:${PORT}`;
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-//const nannyRouter = require('./routes/post');
-//const clientRouter = require('./routes/user');
+const requestRouter = require('./routes/requestRoutes');
+const messageRouter = require('./routes/messageRoutes');
+const userRouter = require('./routes/userRoutes');
 
 //middleware
 app.use(cors());
 app.use(bodyParser.json());
 
-//app.use("/clients", clientRouter);
-//app.use("/nannies", postRouter);
+app.use('/api/requests', requestRouter);
+app.use('/api/messages', messageRouter);
+app.use('/api/users', userRouter);
 
 app.get("/api/nannies", (req, res) => {
     res.send("Nannies server is running");
