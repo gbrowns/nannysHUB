9f678c1d23220d18ea981ee6c7f2b4f8e0a19718
Godwin254 <godwinbee15@gmail.com>
2022-08-25 14:51:36 +0300

    add nanny routes

--- server/src/index.js
+++ server/src/index.js
@@ -7,11 +7,20 @@ const LOCALHOST = `http://localhost:${PORT}`;
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+//const nannyRouter = require('./routes/post');
+//const clientRouter = require('./routes/user');
+
 //middleware
 app.use(cors());
 app.use(bodyParser.json());
 
+//app.use("/clients", clientRouter);
+//app.use("/nannies", postRouter);
 
-app.listen(PORT, (req, res)=>{
+app.get("/api/nannies", (req, res) => {
+    res.send("Nannies server is running");
+})
+
+app.listen(PORT, () =>{
     console.log(`Server is running on ${LOCALHOST}`);
 });
