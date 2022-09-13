2cab1d3489437314d3d4df0bd368acab2dc68fd2
Godwin254 <godwinbee15@gmail.com>
2022-09-13 19:32:05 +0300

    add send email on job application for Nanny

--- server/src/controllers/nannyController.js
+++ server/src/controllers/nannyController.js
@@ -1,6 +1,6 @@
 //require services
 const nannyService = require("../services/nannyService");
-
+const sendEmail = require('../utils/sendEmail')
 //handle users
 const getAllNannies = async (req, res) => {
     try {
@@ -33,14 +33,24 @@ const createNewNanny = async (req, res) => {
     const { firstname, lastname, email, phone, city, location, gender, age, employment_status, agreement_type, availableTime, work_type, message} = req.body; //update later to include more fields
     const newNanny = { firstname, lastname, email, phone, city, location, gender, age, employment_status, agreement_type, availableTime,work_type, message, state: "pending"};
 
+    const nannyEmail = {
+        email: email,
+        subject: "Request To Become a Nanny",
+        message: `Hello ${firstname} ${lastname},
+        Your request to become a nanny has been received. We will get back to you shortly.
+        Thank you!`
+    }
+
     //check inputs are in place
     if (!firstname || !lastname || !email || !phone || !city || !location || !gender || !age || !employment_status || !work_type || !agreement_type || !availableTime || !message) {
         res.status(400).json({ status: "FAILED", message: "Please fill out all fields" });
     }
     
     try {
+
         const createdNanny = await nannyService.createNewNanny(newNanny);
         res.status(201).send({ status: "ok", data: createdNanny });
+        sendEmail(nannyEmail);
     } catch (err) {
         res.status(500).json({ message: err.message });
         console.log(err)
