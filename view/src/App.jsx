947ba154f0a63caece56f89e1b57f5f46bb5c6db
Godwin254 <godwinbee15@gmail.com>
2022-12-29 23:42:31 +0300

    featg: add dashboard UI

--- view/src/App.jsx
+++ view/src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
 import './App.scss'
+import SideNavigation from './components/SideNavigation'
+import AdminDashboard from './pages/AdminDashboard'
 
 function App() {
 
   return (
     <div className="App">
+      <SideNavigation />
+      <AdminDashboard />
        
     </div>
   )
--- view/src/pages/AdminDashboard.jsx
+++ view/src/pages/AdminDashboard.jsx
@@ -2,7 +2,35 @@ import React from 'react'
 
 function AdminDashboard() {
   return (
-    <div className="admin-dashboard">
+    <div className="dashboard">
+      <h1>Dashboard</h1>
+
+      <div className="dashboard_content">
+        <div className='card'>
+          <h3>Nannies Hub Kenya</h3>
+        </div>
+        <div className='card'>
+          <span>Icon</span>
+          <h4>Applications</h4>
+          <h1>{25}</h1>
+        </div>
+        <div className='card'>
+          <span>Icon</span>
+          <h4>Requests</h4>
+          <h1>{25}</h1>
+        </div>
+        <div className='card'>
+          <span>Icon</span>
+          <h4>Nannies</h4>
+          <h1>{25}</h1>
+        </div>
+        <div className='card'>
+          <span>Icon</span>
+          <h4>Nannies</h4>
+          <h1>{25}</h1>
+        </div>
+
+      </div>
 
     </div>
   )
