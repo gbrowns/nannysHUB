1e65ef213694c6e07c2d128a261bf3ab4397125a
Godwin254 <godwinbee15@gmail.com>
2022-12-29 23:17:39 +0300

    feat: add  implementation for admin sidebar navigation

---
+++ view/src/components/SideNavigation.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+//logo
+//side navigation
+
+function SideNavigation() {
+     const year = new Date().getFullYear();
+  return (
+    <div className='sidebar'>
+     <h1>LOGO</h1>
+
+     
+     <a href="#">Dashboard</a>
+     <a href="#">Nannies</a>
+     <a href="#">Applications</a>
+     <a href="#">Requests</a>     
+     <a href="#">Payments</a>     
+     <a href="#">Notifications</a>
+
+
+     <button>Logout</button>
+     <span>&copy; {year} | Nannies Hub </span>
+    </div>
+  )
+}
+
+export default SideNavigation;
