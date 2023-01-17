8b32a8d09d0b5dc0b44ea5dafff1ed0b46c7fd36
Brown <45181428+Godwin254@users.noreply.github.com>
2023-01-17 17:49:10 +0000

    add payment success page

--- view/src/pages/SuccessPaymentPage.jsx
+++ view/src/pages/SuccessPaymentPage.jsx
@@ -5,11 +5,10 @@ import {BiCopyAlt} from 'react-icons/bi';
 function SuccessPaymentPage() {
   return (
     <div className="successPayment">
-          <RiMoneyDollarCircleFill className='successPayment_icon' />
-          <h2>Payment Successful</h2>
-          <p>Thank you for using <a href='/'>Nannies HUb KE</a></p>
-          <p>Mpesa Reference: <span>123456789 <BiCopyAlt className="icon" /></span></p>
-
+      <RiMoneyDollarCircleFill className='successPayment_icon' />
+      <h2>Payment Successful</h2>
+      <p>Thank you for using <a href='/'>Nannies HUb KE</a></p>
+      <p>Mpesa Reference: <span>123456789 <BiCopyAlt className="icon" /></span></p>
     </div>
   )
 }
